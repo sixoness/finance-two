a27543c3751874601fbcddf4a2a1a05714689f3a
sixone <931039665@qq.com>
2018-11-13 12:33:12 +0800

    模板更新

---
+++ public/javascripts/about.js
@@ -0,0 +1,7 @@
+// $().ready(function(){
+// //    $("#dsj").click(function(){
+// //        alert("888")
+// //         $("#gyth").css("display","none")
+// //         $("#dsj").css("display","block")
+// //    })
+// });
---
+++ public/javascripts/public.js
@@ -0,0 +1,103 @@
+
+
+//header
+inCludeScript ('../public/javascripts/jquery.min.js');
+inCludeScript ('../public/javascripts/about.js');
+
+inCludeScript ('../public/javascripts/jquery.once.js');
+inCludeScript ('../public/javascripts/drupal.js');
+inCludeScript ('../public/javascripts/bootstrap.js' );
+inCludeScript ('../public/javascripts/extlink.js');
+inCludeScript ('../public/javascripts/owl.carousel.js');
+inCludeScript ('../public/javascripts/shCore.js' );
+inCludeScript ('../public/javascripts/webform.js');
+inCludeScript ('../public/javascripts/modernizr.js');
+inCludeScript ('../public/javascripts/jquery.themepunch.tools.min.js');
+inCludeScript ('../public/javascripts/jquery.themepunch.revolution.js');
+inCludeScript ('../public/javascripts/jquery.SuperSlide.2.1.1.js' );
+inCludeScript ('../public/javascripts/isotope.pkgd.js');
+
+inCludeScript ('../public/javascripts/jquery.magnific-popup.js');
+inCludeScript ('../public/javascripts/jquery.appear.js');
+inCludeScript ('../public/javascripts/jquery.countTo.js');
+inCludeScript ('../public/javascripts/jquery.parallax-1.1.3.js');
+inCludeScript ('../public/javascripts/jquery.validate.js');
+inCludeScript ('../public/javascripts/jquery.validate.js');
+inCludeScript ('../public/javascripts/template.js' );
+inCludeScript ('../public/javascripts/custom.js' );
+
+//余额宝
+inCludeScript ('../public/javascripts/emotion-index-num.js');
+inCludeScript ('../public/javascripts/stopExecutionOnTimeout.js');
+inCludeScript ('../public/javascripts/countUp.js' );
+inCludeScript ('../public/javascripts/emotion-index-num.js');
+inCludeScript ('../public/javascripts/jquery.circliful.js');
+inCludeScript ('../public/javascripts/d3.js');
+inCludeScript ('../public/javascripts/c3.js');
+inCludeScript ('../public/javascripts/circleDonutChart.js');
+
+inCludeScript ('../public/javascripts/moment.js');
+inCludeScript ('../public/javascripts/daterangepicker.js');
+inCludeScript ('../public/javascripts/yuebao.js');
+
+
+//关于我们
+// inCludeScript ('../public/javascripts/jia.js');
+
+
+
+
+
+// inCludeScript ('../public/javascripts/_progress.js');
+// inCludeScript ('../public/javascripts/ajax.js');
+// inCludeScript ('../public/javascripts/analytics.js');
+// inCludeScript ('../public/javascripts/auto-submit.js' );
+// inCludeScript ('../public/javascripts/base.js' );
+// inCludeScript ('../public/javascripts/better_exposed_filters.js');
+// inCludeScript ('../public/javascripts/bootstrap.min.js');
+
+
+
+
+
+
+// inCludeScript ('../public/javascripts/drupal.js');
+// inCludeScript ('../public/javascripts/echarts.js');
+
+// inCludeScript ('../public/javascripts/html5shiv.min.js');
+// inCludeScript ('../public/javascripts/html5shiv-printshiv.js');
+// inCludeScript ('../public/javascripts/isotope.pkgd.js');
+
+
+
+// inCludeScript ('../public/javascripts/jquery.cookie.js');
+// inCludeScript ('../public/javascripts/jquery.countTo.js');
+// inCludeScript ('../public/javascripts/jquery.isotope.min.js');
+// inCludeScript ('../public/javascripts/jquery.form.min.js');
+// inCludeScript ('../public/javascripts/jquery.min1.11.0.js');
+// inCludeScript ('../public/javascripts/jquery.magnific-popup.js' );
+
+// inCludeScript ('../public/javascripts/jquery.mobile.custom.min.js');
+// inCludeScript ('../public/javascripts/jquery.once.js');
+// inCludeScript ('../public/javascripts/jquery.parallax-1.1.3.js');
+// inCludeScript ('../public/javascripts/jquery.SuperSlide.2.1.1.js');
+
+// inCludeScript ('../public/javascripts/jquery.themepunch.plugins.min.js');
+// inCludeScript ('../public/javascripts/jquery.themepunch.revolution.js');
+// inCludeScript ('../public/javascripts/jquery.appear.js' );
+
+
+// inCludeScript ('../public/javascripts/drupal.js');
+// inCludeScript ('../public/javascripts/echarts.js');
+
+// inCludeScript ('../public/javascripts/html5shiv.min.js');
+// inCludeScript ('../public/javascripts/html5shiv-printshiv.js');
+// inCludeScript ('../public/javascripts/isotope.pkgd.js');
+// inCludeScript ('../public/javascripts/jquery.appear.js' );
+
+
+
+function inCludeScript(url) {
+	document.write('<script type="text/javascript" src="js/' + url + '"> </script>');
+	return false;
+}
